b36216d3fab11416237400d0fb12bc6c9cada0e3
Zaden Ngobeni <zangobejhb024@student.wethinkcode.co.za>
2024-12-23 19:57:28 +0200

    addind structure

--- rot13.java
+++ rot13.java
@@ -9,9 +9,42 @@ public class Rot13{
         String first = "abcdefghijklm";
         String second = "nopqrstuvwxyz";
 
-        for (int i; i < first.length; i++){
-            System.out.println(first.charAt(i))
+        String upper1 = "ABCDEFGHIJKLM";
+        String upper2 = "NOPQRSTUVWXYZ";
+
+        // for (int i = 0; i < first.length(); i++){
+        //     System.out.println(first.charAt(i));
+        // }
+
+        if (StringIn(first, "a")){
+            System.out.println("a is in first");
         }
         
     }
+
+    public static boolean StringIn(String word, String letter){
+
+        for (int i = 0; i < word.length(); i++){
+            if (word.charAt(i) == letter.charAt(0)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void rot13(String message){
+
+        //for each word in the message
+        for (int i = 0; i < message.length(); i++){
+
+            //for each letter
+
+            char word = message.charAt(i);
+            // String word_ = ((String)word);
+
+            for(int l =0; l < message.charAt(i).length())
+
+        }
+    }
+
 }
