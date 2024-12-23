542d9812c43be56f8eac09cf3c67a8b9aa12158b
Zaden Ngobeni <zangobejhb024@student.wethinkcode.co.za>
2024-12-23 19:28:31 +0200

    start

---
+++ rot13.java
@@ -0,0 +1,17 @@
+public class Rot13{
+
+    public static void main(String[] args){
+
+        // lets begin!!
+        // see you you guys when I am finished
+
+
+        String first = "abcdefghijklm";
+        String second = "nopqrstuvwxyz";
+
+        for (int i; i < first.length; i++){
+            System.out.println(first.charAt(i))
+        }
+        
+    }
+}
